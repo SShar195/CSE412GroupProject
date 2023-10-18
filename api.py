3e010471f4ac2b26ac9fd8cb6c5e5b16203915b9
iamchart <hartcolton4@outlook.com>
2023-10-18 12:00:39 -0700

    added Constants.py

--- api.py
+++ api.py
@@ -1,15 +1,13 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
-from datetime import date
-import json
 
 class API:
     @staticmethod
-    def getPageSource() -> BeautifulSoup:
+    def getPageSource(link:str) -> BeautifulSoup:
         driver = webdriver.Chrome()
-        driver.get("https://www.fandango.com/phoenix_az_movietimes?date=" + str(date.today()))
-        time.sleep(2)
+        driver.get(link)
+        time.sleep(1)
         pageSource = driver.page_source
         soup = BeautifulSoup(pageSource, "html.parser")
         return soup
--- theater.py
+++ theater.py
@@ -1,32 +1,34 @@
 from api import API
+from constants import Constants
 import json
+from datetime import date
 
 class Theater:
     Theaters = []
+    def getTheaters(Theaters ,tag):
+        with open('theater.json', 'r') as json_file:
+            Theaters = json.load(json_file)
+
+        for i in tag:
+            theaterId = i['data-theater-id']
+            if theaterId not in Theaters:
+                name = i.find("a").text.strip()
+                span = i.find(class_="fd-theater__address-wrap dark__text--secondary").text
+                streetAddress = ""
+                for j in span.split("\n"):
+                    if(j.strip()):
+                        streetAddress += " " + j.strip()
+                data = {
+                    "theaterId" : theaterId,
+                    "name" : name,
+                    "streetAddress" : streetAddress 
+                }
+                Theaters.append(data)
+                with open('theater.json', 'w') as json_file:
+                    json.dump(Theaters, json_file, indent=1)
     
-    soup = API.getPageSource()
-    ulTag = soup.find(class_="fd-showtimes js-theaterShowtimes-loading").find_all("li", class_="fd-theater")
-
-    with open('theater.json', 'r') as json_file:
-        Theaters = json.load(json_file)
-
-    for i in ulTag:
-        theaterId = i['data-theater-id']
-        name = i.find("a").text.strip()
-        span = i.find(class_="fd-theater__address-wrap dark__text--secondary").text
-        streetAddress = ""
-        for j in span.split("\n"):
-            if(j.strip()):
-                streetAddress += " " + j.strip()
-        data = {
-            "theaterId" : theaterId,
-            "name" : name,
-            "streetAddress" : streetAddress 
-        }
-
-        if data['theaterId'] not in Theaters:
-            Theaters.append(data)
-
-    with open('theater.json', 'w') as json_file:
-        json.dump(Theaters, json_file, indent=1)
+    for i in Constants.theaterLinkMap.values():
+        soup = API.getPageSource(i + "?date=" + str(date.today()))
+        ulTag = soup.find(class_="fd-showtimes js-theaterShowtimes-loading").find_all("li", class_="fd-theater")
+        getTheaters(Theaters, ulTag)
             
