945e504183bbcb2cf8a2cfdcc798da3644d3a2c0
Tyler <tjmorga7@asu.edu>
2023-11-29 21:53:04 -0700

    Basic Function for movies!

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
-import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def homepage():
     return render_template('homepage.html')
+
+@app.route('/movie_info.html')
+def movie_info():
+    # You can pass movie data dynamically here if needed
+    return render_template('movie_info.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
