32c0dc488bc92b2912f5bcef22e8c4f6d11acea2
SShar195 <fyrepyryte@gmail.com>
2023-11-30 11:38:47 -0700

    modified:   app.py 	modified:   templates/movie_info.html

--- app.py
+++ app.py
@@ -13,49 +13,49 @@ username_secret = os.getenv("DATABASE_USERNAME") # Make sure you have a .env fil
 password_secret = os.getenv("DATABASE_PASSWORD")
 database_secret = os.getenv("DATABASE_NAME")
 
-connection = psycopg2.connect(host='localhost', database = database_secret, user = username_secret, password = password_secret)
-cursor = connection.cursor()
+# connection = psycopg2.connect(host='localhost', database = database_secret, user = username_secret, password = password_secret)
+# cursor = connection.cursor()
 
-ticket_table = '''
-    CREATE TABLE IF NOT EXISTS ticket (
-        movieID VARCHAR(6),
-        theaterID VARCHAR(5),
-        showtime VARCHAR(5),
-        seat VARCHAR(3),
-        isMatinee BOOLEAN,
-        price VARCHAR(6)
-    )'''
+# ticket_table = '''
+#     CREATE TABLE IF NOT EXISTS ticket (
+#         movieID VARCHAR(6),
+#         theaterID VARCHAR(5),
+#         showtime VARCHAR(5),
+#         seat VARCHAR(3),
+#         isMatinee BOOLEAN,
+#         price VARCHAR(6)
+#     )'''
 
-movie_table = '''
-    CREATE TABLE IF NOT EXISTS movie (
-        title VARCHAR(124),
-        movieID VARCHAR(6)
-    )'''
+# movie_table = '''
+#     CREATE TABLE IF NOT EXISTS movie (
+#         title VARCHAR(124),
+#         movieID VARCHAR(6)
+#     )'''
 
-theater_table = '''
-    CREATE TABLE IF NOT EXISTS theater (
-        theaterID VARCHAR(5),
-        name VARCHAR(124),
-        StreetAddress VARCHAR(248)
-    )'''
+# theater_table = '''
+#     CREATE TABLE IF NOT EXISTS theater (
+#         theaterID VARCHAR(5),
+#         name VARCHAR(124),
+#         StreetAddress VARCHAR(248)
+#     )'''
 
-cursor.execute(ticket_table)
-cursor.execute(movie_table)
-cursor.execute(theater_table)
+# cursor.execute(ticket_table)
+# cursor.execute(movie_table)
+# cursor.execute(theater_table)
 
-with open('ticket.csv', 'r') as f:
-    next(f) # Skip the header row.
-    cursor.copy_from(f, 'ticket', sep=';')
+# with open('ticket.csv', 'r') as f:
+#     next(f) # Skip the header row.
+#     cursor.copy_from(f, 'ticket', sep=';')
 
-with open('theater.csv', 'r') as f:
-    next(f)
-    cursor.copy_from(f, 'theater', sep=';')
+# with open('theater.csv', 'r') as f:
+#     next(f)
+#     cursor.copy_from(f, 'theater', sep=';')
 
-with open('movie.csv', 'r') as f:
-    next(f)
-    cursor.copy_from(f, 'movie', sep=';')
+# with open('movie.csv', 'r') as f:
+#     next(f)
+#     cursor.copy_from(f, 'movie', sep=';')
 
-connection.commit()
+# connection.commit()
 
 @app.route('/')
 def homepage():
